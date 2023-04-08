038f7f8798803f4e00477863d4518512630d71da
ilgaribaldi <marcelovillarrealx@outlook.com>
2023-04-08 15:40:09 -0600

    added navigation

--- src/App.js
+++ src/App.js
@@ -1,96 +1,106 @@
-import React, { useState, useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Layout, Menu } from 'antd'
+import { BrowserRouter as Router, NavLink, Route, Routes, useLocation } from 'react-router-dom'
 import About from './views/about/index'
 import Projects from './views/projects/index'
 import Contact from './views/contact/index'
 
 const { Header, Content, Footer } = Layout
 
+const ScrollToTop = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+};
+
 function App() {
-  const [activeTab, setActiveTab] = useState('1')
+  const [selectedKeys, setSelectedKeys] = useState(['/about'])
+
+  const UpdateMenuItem = () => {
+    const location = useLocation()
+
+    useEffect(() => {
+      setSelectedKeys([location.pathname])
+    }, [location.pathname])
+
+    return null
+  }
 
   useEffect(() => {
     document.title = "Marcelo's Portfolio"
   }, [])
 
-  const handleMenuClick = (e) => {
-    setActiveTab(e.key)
-  }
-
-  const renderContent = () => {
-    switch (activeTab) {
-      case '1':
-        return <About />
-      case '2':
-        return <Projects />
-      case '3':
-        return <Contact />
-      default:
-        return <About />
-    }
-  }
-
   return (
-    <Layout>
-      ;
-      <Header
-        style={{
-          position: 'fixed',
-          zIndex: 1,
-          width: '100%',
-        }}
-      >
-        <div
+    <Router>
+      <ScrollToTop/>
+      <UpdateMenuItem />
+      <Layout>
+        <Header
           style={{
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-            height: '100%',
+            position: 'fixed',
+            zIndex: 1,
+            width: '100%',
           }}
         >
-          <Menu
-            theme="dark"
-            mode="horizontal"
-            defaultSelectedKeys={['1']}
-            onClick={handleMenuClick}
-            style={{ display: 'flex', justifyContent: 'center', flexGrow: 1 }}
+          <div
+            style={{
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+              height: '100%',
+            }}
           >
-            <Menu.Item
-              key="1"
-              style={{ fontWeight: 'bold', letterSpacing: '0.05rem' }}
+            <Menu
+              theme="dark"
+              mode="horizontal"
+              selectedKeys={selectedKeys}
+              style={{ display: 'flex', justifyContent: 'center', flexGrow: 1 }}
             >
-              About
-            </Menu.Item>
-            <Menu.Item
-              key="2"
-              style={{ fontWeight: 'bold', letterSpacing: '0.05rem' }}
-            >
-              Projects
-            </Menu.Item>
-            <Menu.Item
-              key="3"
-              style={{ fontWeight: 'bold', letterSpacing: '0.05rem' }}
-            >
-              Contact
-            </Menu.Item>
-          </Menu>
-        </div>
-      </Header>
-      <Content
-        style={{
-          padding: '0 20px',
-          marginTop: 64,
-          background: 'linear-gradient(120deg, #fdfbfb 0%, #ebedee 100%)',
-        }}
-      >
-        <div style={{ background: '#fff', padding: 24, minHeight: 380 }}>
-          {renderContent()}
-        </div>
-      </Content>
-      <Footer style={{ textAlign: 'center' }}>
-        Marcelo Villarreal ©{new Date().getFullYear()} Created with Ant Design
-      </Footer>
-    </Layout>
+              <Menu.Item
+                key="/about"
+                style={{ fontWeight: 'bold', letterSpacing: '0.05rem' }}
+              >
+                <NavLink to="/about" end>About</NavLink>
+              </Menu.Item>
+              <Menu.Item
+                key="/projects"
+                style={{ fontWeight: 'bold', letterSpacing: '0.05rem' }}
+              >
+                <NavLink to="/projects">Projects</NavLink>
+              </Menu.Item>
+              <Menu.Item
+                key="/contact"
+                style={{ fontWeight: 'bold', letterSpacing: '0.05rem' }}
+              >
+                <NavLink to="/contact">Contact</NavLink>
+              </Menu.Item>
+            </Menu>
+          </div>
+        </Header>
+        <Content
+          style={{
+            padding: '0 20px',
+            marginTop: 64,
+            background: 'linear-gradient(120deg, #fdfbfb 0%, #ebedee 100%)',
+          }}
+        >
+          <div style={{ background: '#fff', padding: 24, minHeight: 380 }}>
+            <Routes>
+              <Route path="/about" element={<About />} />
+              <Route path="/projects" element={<Projects />} />
+              <Route path="/contact" element={<Contact />} />
+            </Routes>
+          </div>
+        </Content>
+        <Footer style={{ textAlign: 'center' }}>
+          Marcelo Villarreal ©{new Date().getFullYear()} Created with Ant Design
+        </Footer>
+      </Layout>
+    </Router>
   )
 }
 
